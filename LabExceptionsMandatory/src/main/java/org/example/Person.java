fe95418c9b83da577b21d34bf7afe6ce708145d3
peterParker79 <trypeterparker@gmail.com>
2025-04-10 17:08:30 +0200

    class Person and PersonsList

--- LabExceptionsMandatory/src/main/java/org/example/Person.java
+++ LabExceptionsMandatory/src/main/java/org/example/Person.java
@@ -1,4 +1,80 @@
 package org.example;
 
 public class Person {
+    private int id;
+    private String name; //a string formatted as "firstName lastName
+    private int age;
+    private String occupation;
+
+
+    public Person(int id, String name, int age, String occupation) {
+        setId(id);
+        setName(name);
+        setAge(age);
+        setOccupation(occupation);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge()  {
+
+
+        return age;
+    }
+
+    public void setAge(int age)  throws IllegalArgumentException {
+        if (age<0){
+            throw new IllegalArgumentException("Valor de edad incorrecto");
+        }
+
+        this.age = age;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public boolean equals(Person p){
+        if (this.name.equalsIgnoreCase(p.getName())
+                && this.age==p.getAge()
+                && this.occupation.equalsIgnoreCase(p.getOccupation())){
+            return true;
+        }
+        return false;
+    }
+
+    //check name format name  space  surname
+    public boolean checkCorrectName (String name){
+      String[] nameSurname= name.trim().split("");
+      if (nameSurname.length != 2){return false;}
+      return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", occupation='" + occupation + '\'' +
+                '}';
+    }
 }
--- LabExceptionsMandatory/src/main/java/org/example/PersonsList.java
+++ LabExceptionsMandatory/src/main/java/org/example/PersonsList.java
@@ -1,4 +1,47 @@
 package org.example;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
 public class PersonsList {
+private HashMap <String, Person> personsList;
+
+    public PersonsList() {
+       personsList = new HashMap<String, Person>();
+    }
+
+    public Person findByName(String name) throws IllegalArgumentException{
+    if(!(checkCorrectName(name)){
+        throw new IllegalArgumentException();
+        }
+    // return person if exists.
+    if (personsList.containsKey(name)){return personsList.get(name);}
+    }
+
+
+    public Person clone(Person p){
+        Random random= new Random();
+        int newId= random.nextInt();
+        Person person= new Person(newId,p.getName(),
+                p.getAge(),p.getOccupation());
+        return person;
+    }
+
+    public boolean checkCorrectName (String name){
+        String[] nameSurname= name.trim().split("");
+        if (nameSurname.length != 2){return false;}
+        return true;
+    }
+
+
+    public void writePersonToFile(String fileName, Person p) throws IOException {
+        FileWriter writer = new FileWriter(fileName);
+        writer.write(p.toString());
+
+    }
+
+
 }
